22a019d3bb05026f1b91580cc9fe8362f52b1bd7
JoseMauricioNet <mauricion751@gmail.com>
2020-12-14 17:18:52 +0000

    Adicionei os comandos.

--- main.py
+++ main.py
@@ -3,7 +3,13 @@
 
 def main():
     """Função principal que será rodada quando o script for passado para o interpretador."""
-    # COLOQUE SEU CÓDIGO AQUI
+    import math
+    a=input('Digite um número:')
+    a=float(a)
+    b=input('Digite outro número:')
+    b=float(b)
+    numeros=[a,b]
+    print(f'{max(numeros)}')
 
 if __name__ == '__main__':
     main()
